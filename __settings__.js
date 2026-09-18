1d96fda0915b689773029aec5dbdbed01a1541a9
keanu4 <87876062+keanupaesler@users.noreply.github.com>
2026-09-18 11:18:22 -0700

    initial files

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2593806.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 306611773, 306611774, 306611772 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/306611791/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/306611795/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/306611786/1/ammo.js', 'preload' : true},
+];
